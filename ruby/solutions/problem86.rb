0d1bcc94e98d018d512f3d51942d31137fbe0242
eugen neagoe <eneagoe@gmail.com>
2014-03-03 16:52:36 +0200

    fixed/solved problem 86 (ruby)

--- ruby/solutions/problem86.rb
+++ ruby/solutions/problem86.rb
@@ -5,20 +5,24 @@ class Problem86 < Problem
 
   def solve
 
-    n, target = 10, 2000
-    while true
-      n += 1
-      count = 0
-      (1..n).each do |h|
-        (h..n).each do |w|
-          (w..n).each do |l|
-            s = Math.sqrt(l**2 + (h+w)**2)
-            count += 1 if s % 1 == 0
-          end
+    m = 1
+    count = 0
+    target = 1_000_000
+
+    while count < target
+
+      m += 1
+
+      (3..2*m).each do |wh|
+        d = Math.sqrt(wh**2 + m**2)
+        if d % 1 == 0
+          count += (wh <= m) ? wh / 2 : 1 + (m - (wh + 1) / 2)
         end
       end
-      break n if count >= target
+
     end
+
+    m
 
   end
 
