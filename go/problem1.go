e1c0a7e711743f02a5f03c119ae7b217daf5eae4
eugen neagoe <eneagoe@gmail.com>
2015-12-01 16:28:03 +0200

    Format Go solution for problem 1 with gofmt

--- go/problem1.go
+++ go/problem1.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 
-  result := 0
+	result := 0
 
-  for i := 0; i < 1000; i++ {
-    if i % 3 == 0 || i % 5 == 0 {
-      result += i
-    }
-  }
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			result += i
+		}
+	}
 
-  fmt.Println(result)
+	fmt.Println(result)
 
 }
