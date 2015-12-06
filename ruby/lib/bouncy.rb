10f986311df008d253a15e2e5892f3e10f410028
eugen neagoe <eneagoe@gmail.com>
2015-12-06 19:52:47 +0200

    Improve readability and performance of bouncy checks

--- ruby/lib/bouncy.rb
+++ ruby/lib/bouncy.rb
@@ -2,8 +2,8 @@ require "digits"
 
 def bouncy?(n)
   digit_pairs = digits(n).each_cons(2)
-  increasing = digit_pairs.reduce(true) { |s, (a, b)| s && (a <= b) }
-  decreasing = digit_pairs.reduce(true) { |s, (a, b)| s && (a >= b) }
+  increasing = !digit_pairs.any? { |a, b| a > b }
+  decreasing = !digit_pairs.any? { |a, b| a < b }
 
   !increasing && !decreasing
 end
