2240f097e7c3e5a54585093b96d31834432fcdda
eugen neagoe <eneagoe@gmail.com>
2019-09-11 18:54:56 +0300

    Add optimised Ruby solution for 39

--- ruby/solutions/problem39.rb
+++ ruby/solutions/problem39.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + "/../lib")
 
-# brute-force, extremely slow, but still within the 1m limit
 # Solves http://projecteuler.net/problem=39
 class Problem
+  # brute-force, extremely slow, but still within the 1m limit
   def self.solution_1
     (0..1000).map do |p|
       solutions = 0
@@ -15,6 +15,23 @@ class Problem
 
       solutions
     end.each_with_index.max[1]
+  end
+
+  # take any two sides, find a third side that will form a right triangle,
+  # and increase the count for the perimeter by 1, and finally take the max count
+  def self.solution_2
+    p = Hash.new(0)
+
+    (1..998).each do |a|
+      (a..998).each do |b|
+        break if a + b > 1000
+        c = Math.sqrt(a**2 + b**2).to_i
+        break if a + b + c > 1000
+        p[a + b + c] += 1 if c**2 == a**2 + b**2
+      end
+    end
+
+    p.max_by { |k, v| v }.first
   end
 end
 
